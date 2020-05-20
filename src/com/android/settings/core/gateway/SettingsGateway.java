ezio84/abc-n_packages_apps_Settings

Allow to quickly open Running Services from QS panel [2/2]
@Mazda-- : Remove text from actionbar / Use overflow menu

ezio: Running services: update actionbar title accordingly

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -56,6 +56,7 @@ import com.android.settings.applications.PictureInPictureDetails;
 import com.android.settings.applications.PictureInPictureSettings;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.WriteSettingsDetails;
@@ -247,6 +248,7 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             AbcSettings.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -285,5 +287,6 @@ public class SettingsGateway {
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.AbcSettingsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }

fzkangga/DerpFest-Pie_platform_packages_apps_Settings

Change-Id: I8f1bc70d75aa2f07031f30a2fd408e220b8574e6
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -44,6 +44,7 @@ import com.android.settings.applications.DirectoryAccessDetails;
 import com.android.settings.applications.ManageDomainUrls;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.VrListenerSettings;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
@@ -310,6 +311,7 @@ public class SettingsGateway {
             PulseSettingsActivity.class.getName(),
             BatteryLightSettings.class.getName(),
             CustomHeaders.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -355,6 +357,7 @@ public class SettingsGateway {
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
             Settings.PulseSettingsActivity.class.getName(),
-            Settings.CustomHeadersActivity.class.getName()
+            Settings.CustomHeadersActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName()
     };
 }