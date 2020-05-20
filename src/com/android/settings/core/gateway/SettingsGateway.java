AquariOS/packages_apps_Settings

Long press custom header to open custom header settings [2/2]
Change-Id: I5ce74197ef3a980a937811f8c0fe2d8d2d5020c8

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -142,6 +142,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.aquarios.coralreef.CoralReef;
+import com.aquarios.coralreef.fragments.CustomHeaders;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 public class SettingsGateway {
@@ -274,7 +275,8 @@ public class SettingsGateway {
             SmartbarSettings.class.getName(),
             FlingSettings.class.getName(),
             PulseSettings.class.getName(),
-            SwipeUpGestureSettings.class.getName()
+            SwipeUpGestureSettings.class.getName(),
+            CustomHeaders.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -318,6 +320,7 @@ public class SettingsGateway {
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(), 
             Settings.PulseSettingsActivity.class.getName(),
-            Settings.StockNavigationActivity.class.getName()
+            Settings.StockNavigationActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }

fzkangga/DerpFest-Pie_platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -143,6 +143,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.aosip.owlsnest.OwlsNestSettings;
+import com.aosip.owlsnest.quicksettings.CustomHeaders;
 import com.aosip.owlsnest.advanced.SystemCategory;
 import com.aosip.owlsnest.buttons.NavigationCategory;
 import com.aosip.owlsnest.buttons.PowermenuCategory;
@@ -308,6 +309,7 @@ public class SettingsGateway {
             FlingSettingsActivity.class.getName(),
             PulseSettingsActivity.class.getName(),
             BatteryLightSettings.class.getName(),
+            CustomHeaders.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -352,6 +354,7 @@ public class SettingsGateway {
             Settings.NavigationSettingsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
-            Settings.PulseSettingsActivity.class.getName()
+            Settings.PulseSettingsActivity.class.getName(),
+            Settings.CustomHeadersActivity.class.getName()
     };
 }